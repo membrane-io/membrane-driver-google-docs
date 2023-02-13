f7ff1db2408d91c01491044caee66ec79a926b09
Juan Campa <juancampa@gmail.com>
2023-02-12 23:00:53 -0500

    Implement Membrane Auth. Improve endpoint HTML

--- index.ts
+++ index.ts
@@ -1,52 +1,20 @@
-import { nodes, state, root } from 'membrane';
-import ClientOAuth2 from 'client-oauth2';
-import fetch from 'node-fetch';
-
-type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
-
-async function api(method: Method, domain: string, path: string, query?: any, body?: string) {
-  if (!state.accessToken) {
-    throw new Error("You must authenticated to use this API. Visit the program's /auth endpoint");
-  }
-  if (query) {
-    Object.keys(query).forEach(key => query[key] === undefined ? delete query[key] : {});
-  }
-  const querystr = query && Object.keys(query).length ? `?${new URLSearchParams(query)}`: '';
-
-  if (state.accessToken.expired()) {
-    console.log('Refreshing access token...');
-    state.accessToken = await state.accessToken.refresh();
-  }
-
-  const req = state.accessToken.sign({
-    method,
-    url: `https://${domain}/${path}${querystr}`,
-    body
-  });
-  return await fetch(req.url, req)
-}
-
-if (state.auth) {
-  state.auth.request = oauthRequest;
-}
+import { nodes, state, root } from "membrane";
+import * as util from "./util";
+import { api, createAuthClient } from "./util";
 
 export const Root = {
-  blob() {
-    // return JSON.stringify(state.accessToken?.data, null, 2);
+  authId() {
+    return "google-docs";
   },
-  status() {
-    if (!state.auth) {
-      return 'Not configured';
-    } else if (!state.accessToken) {
-      return `Please [authenticate with Google](${state.endpointUrl}/auth)`;
-    } else {
-      return `Ready`;
-    }
+  async status() {
+    return await util.authStatus();
   },
   parse({ args: { name, value } }) {
     switch (name) {
       case "documentUrl": {
-        const id = value.match(new RegExp('/document/d/([a-zA-Z0-9-_]+)', 'i'))?.[1];
+        const id = value.match(
+          new RegExp("/document/d/([a-zA-Z0-9-_]+)", "i")
+        )?.[1];
         if (id) {
           return [root.documents.one({ id })];
         }
@@ -57,93 +25,32 @@ export const Root = {
   },
   documents: () => ({}),
   checkStatus: async () => {
-    const res = await api('GET', 'www.googleapis.com', `drive/v3/files`, { pageSize: 1 });
+    const res = await api("GET", "www.googleapis.com", `drive/v3/files`, {
+      pageSize: 1,
+    });
     return res.status === 200;
-  }
-}
-
-// Helper function to produce nicer HTML
-function html(body: string) {
-  return `
-  <!DOCTYPE html>
-  <head>
-    <title>Google Docs Driver for Membrane</title>
-    <link rel="stylesheet" href="https://unpkg.com/bootstrap@4.1.0/dist/css/bootstrap-reboot.css">
-  </head>
-  <body style="padding: 0px 15px">
-    <p>
-      <h1>Google Docs Driver for Membrane</h1>
-      ${body}
-    </p>
-  </body>
-  `;
-}
+  },
+};
 
 export async function endpoint({ args: { path, query, headers, body } }) {
+  const link = await nodes.http
+    .authenticated({ api: "google-docs", authId: root.authId })
+    .createLink.$invoke();
   switch (path) {
-    case '/': {
-      return html(`<a href="/auth">Authenticate with Google</a>`);
-    }
-    case '/auth':
-    case '/auth/': {
-      ensureClient();
-      const url = state.auth.code.getUri({
-        query: { access_type: 'offline', prompt: 'consent' } // Request refresh token
-        // TODO: state
-      })
-      return JSON.stringify({ status: 303, headers: { location: url } })
-    }
-    case '/auth/callback': {
-      state.accessToken = await state.auth.code.getToken(`${path}?${query}`)
-      if (state.accessToken?.accessToken) {
-        return html(`Driver configured!`);
-      }
-      return html(`There was an issue acquiring the access token. Check the logs.`);
-    }
+    case "/":
+    case "/auth":
+    case "/auth/":
+    case "/auth/callback":
+      return;
     default:
-      console.log('Unknown Endpoint:', path);
+      return JSON.stringify({ status: 404, body: "Not found" });
   }
 }
 
-async function oauthRequest(method: string, url: string, reqBody: string, headers: any) {
-  const res = await fetch(url, { body: reqBody.toString(), headers, method })
-  const status = res.status;
-  const body = await res.text();
-  return { status, body }
-}
-
-function ensureClient() {
-  const { clientId, clientSecret } = state;
-  if (!clientId || !clientSecret) {
-    throw new Error('You must configure the driver with a client ID and secret');
-  }
-
-  state.auth = new ClientOAuth2(
-    {
-      clientId,
-      clientSecret,
-      accessTokenUri: 'https://oauth2.googleapis.com/token',
-      authorizationUri: 'https://accounts.google.com/o/oauth2/v2/auth',
-      redirectUri: `${state.endpointUrl}/auth/callback`,
-      // The 'drive.readonly' scope is required to list Google Docs files
-      scopes: ['https://www.googleapis.com/auth/documents', 'https://www.googleapis.com/auth/drive.readonly']
-    },
-    oauthRequest
-  )
-}
-
-export async function configure({ args: { clientId, clientSecret, token } }) {
-  state.endpointUrl = await nodes.endpoint.$get();
+export async function configure({ args: { clientId, clientSecret } }) {
   state.clientId = clientId;
   state.clientSecret = clientSecret;
-  // Token is optional, but it's convenient while working on this driver (and perhaps constantly killing it) to avoid
-  // having to auth with Google over and over
-  if (token) {
-    // If restoring a token, assume it's expired so it gets refreshed right away
-    const data = { ...JSON.parse(token), expires_in: 0 };
-    state.accessToken = state.auth.createToken(null, null, null, data);
-  }
-  ensureClient();
+  await createAuthClient();
 }
 
 type ResolverInfo = {
@@ -159,20 +66,33 @@ type Document = any;
 
 // TODO: Perhaps we can use an ETAG to prevent getting the same content over and over
 const apiGetDocument = async (id: string) => {
-    const res = await api('GET', 'content-docs.googleapis.com', `v1/documents/${encodeURIComponent(id)}`);
-    return await res.json();
-}
+  const res = await api(
+    "GET",
+    "content-docs.googleapis.com",
+    `v1/documents/${encodeURIComponent(id)}`
+  );
+  return await res.json();
+};
 
 // Determines if a query includes any fields that require fetching a given resource. Simple fields is an array of the
 // fields that can be resolved without fetching
-const shouldFetch = (info: ResolverInfo, simpleFields: string[]) => info.fieldNodes
-  .flatMap(({ selectionSet: { selections } }) => { return selections })
-  .some(({ name: { value }}) => !simpleFields.includes(value));
+const shouldFetch = (info: ResolverInfo, simpleFields: string[]) =>
+  info.fieldNodes
+    .flatMap(({ selectionSet: { selections } }) => {
+      return selections;
+    })
+    .some(({ name: { value } }) => !simpleFields.includes(value));
 
 export const DocumentCollection = {
   async create({ args: { title, body } }) {
     const doc = { title, body: body && JSON.parse(body) };
-    const res = await api('POST', 'docs.googleapis.com', `v1/documents`, {}, JSON.stringify(doc));
+    const res = await api(
+      "POST",
+      "docs.googleapis.com",
+      `v1/documents`,
+      {},
+      JSON.stringify(doc)
+    );
     if (res.status >= 300) {
       throw new Error(`Error creating document: ${await res.text()}`);
     }
@@ -181,41 +101,49 @@ export const DocumentCollection = {
   },
   async one({ args: { id }, context, info }) {
     context.documentId = id;
-    if (!shouldFetch(info, ['id'])) {
+    if (!shouldFetch(info, ["id"])) {
       return { id };
     }
     return await apiGetDocument(id);
   },
-  async page({ args, context }) {
-    // TODO: 
-  },
+  // TODO: list documents
+  // async page({ args, context }) {
+  // },
 };
 
 export const Document = {
-  body: ({ obj }) => obj?.body ? JSON.stringify(obj.body) : null,
+  body: ({ obj }) => (obj?.body ? JSON.stringify(obj.body) : null),
   markdown: ({ obj }) => {
     return jsonToMarkdown(obj);
   },
   batchUpdate: async ({ args: { requests }, self }) => {
     const { id } = self.$argsAt(root.documents.one);
-    const res = await api('POST', 'content-docs.googleapis.com', `v1/documents/${encodeURIComponent(id)}:batchUpdate`, {}, requests);
+    const res = await api(
+      "POST",
+      "content-docs.googleapis.com",
+      `v1/documents/${encodeURIComponent(id)}:batchUpdate`,
+      {},
+      requests
+    );
     return await res.json();
   },
   replaceAllText: async ({ args: { text, replaceText, matchCase }, self }) => {
-    const res = await applyUpdate(self, { replaceAllText: { containsText: { text, matchCase }, replaceText } });
+    const res = await applyUpdate(self, {
+      replaceAllText: { containsText: { text, matchCase }, replaceText },
+    });
     return res?.replies?.[0]?.replaceAllText?.occurrencesChanged ?? 0;
   },
   insertText: async ({ args: { text, segmentId, index }, self }) => {
-    const req : any = { insertText: { text } };
+    const req: any = { insertText: { text } };
     if (index != null) {
-      req.insertText.location = { segmentId: segmentId || '', index };
+      req.insertText.location = { segmentId: segmentId || "", index };
     } else {
-      req.insertText.endOfSegmentLocation = { segmentId: segmentId || '' };
+      req.insertText.endOfSegmentLocation = { segmentId: segmentId || "" };
     }
     await applyUpdate(self, req);
   },
   insertBullet: async ({ args: { text, index, bulletPreset }, self }) => {
-    await self.insertText({ text: '\n' + text, index }).$invoke();
+    await self.insertText({ text: "\n" + text, index }).$invoke();
 
     const { id } = self.$argsAt(root.documents.one);
     let startIndex: number;
@@ -225,10 +153,15 @@ export const Document = {
       const content = (await apiGetDocument(id))?.body?.content;
       startIndex = content?.[content.length - 1]?.endIndex - 1;
     }
-    
-    const range = { startIndex, endIndex: startIndex + 1, segmentId: '' };
 
-    const req: any = { createParagraphBullets: { range, bulletPreset: bulletPreset ?? 'BULLET_DISC_CIRCLE_SQUARE' }};
+    const range = { startIndex, endIndex: startIndex + 1, segmentId: "" };
+
+    const req: any = {
+      createParagraphBullets: {
+        range,
+        bulletPreset: bulletPreset ?? "BULLET_DISC_CIRCLE_SQUARE",
+      },
+    };
     await applyUpdate(self, req);
   },
   insertLink: async ({ args: { text, index, url }, self }) => {
@@ -242,17 +175,29 @@ export const Document = {
       const content = (await apiGetDocument(id))?.body?.content;
       endIndex = content?.[content.length - 1]?.endIndex - 1;
     }
-    
-    const range = { startIndex: endIndex - text.length, endIndex, segmentId: '' };
-    const req: any = { updateTextStyle: { textStyle: { link: { url } }, range, fields: "link" } }
-    await applyUpdate(self, req);
-  }
-}
 
-async function applyUpdate(self: any, request: object): Promise<any>{
-  const requests = { requests: [request] }; 
+    const range = {
+      startIndex: endIndex - text.length,
+      endIndex,
+      segmentId: "",
+    };
+    const req: any = {
+      updateTextStyle: { textStyle: { link: { url } }, range, fields: "link" },
+    };
+    await applyUpdate(self, req);
+  },
+};
+
+async function applyUpdate(self: any, request: object): Promise<any> {
+  const requests = { requests: [request] };
   const { id } = self.$argsAt(root.documents.one);
-  const res = await api('POST', 'content-docs.googleapis.com', `v1/documents/${encodeURIComponent(id)}:batchUpdate`, {}, JSON.stringify(requests));
+  const res = await api(
+    "POST",
+    "content-docs.googleapis.com",
+    `v1/documents/${encodeURIComponent(id)}:batchUpdate`,
+    {},
+    JSON.stringify(requests)
+  );
   if (res.status !== 200) {
     throw new Error(`Error applying update: ${await res.text()}`);
   }
@@ -261,7 +206,10 @@ async function applyUpdate(self: any, request: object): Promise<any>{
 
 // Taken from https://github.com/AnandChowdhary/docs-markdown/blob/6bd4f2f4d564bc9f225f03cdbcb42522c924cac3/index.ts
 // It's not perfect but it's a start
-const jsonToMarkdown = (file: any): string => {
+const jsonToMarkdown = (file: any): string | null => {
+  if (!file?.body?.content) {
+    return null;
+  }
   let text = `---
 title: ${file.title}
 documentId: ${file.documentId}
@@ -274,11 +222,11 @@ revisionId: ${file.revisionId}
      */
     if (item.table?.tableRows) {
       // Make a blank header
-        const cells = item.table.tableRows[0]?.tableCells;
-        // Make a blank header
-        text += `|${cells?.map(() => "").join("|")}|\n|${cells
-          ?.map(() => "-")
-          .join("|")}|\n`;
+      const cells = item.table.tableRows[0]?.tableCells;
+      // Make a blank header
+      text += `|${cells?.map(() => "").join("|")}|\n|${cells
+        ?.map(() => "-")
+        .join("|")}|\n`;
       item.table.tableRows.forEach(({ tableCells }) => {
         const textRows: string[] = [];
         tableCells?.forEach(({ content }) => {
@@ -290,7 +238,7 @@ revisionId: ${file.revisionId}
               styleElement(element, styleType)?.replace(/\n+/g, "").trim()
             );
 
-            textRows.push(e.join(''));
+            textRows.push(e.join(""));
           });
         });
         text += `| ${textRows.join(" | ")} |\n`;
@@ -349,13 +297,10 @@ revisionId: ${file.revisionId}
   return text.replace(/\n\s*\n\s*\n/g, "\n\n") + "\n";
 };
 
-const styleElement = (
-  element: any,
-  styleType?: string
-): string | undefined => {
+const styleElement = (element: any, styleType?: string): string | undefined => {
   const text = content(element);
   if (!text || text.length === 0) {
-    return '';
+    return "";
   }
 
   if (styleType === "TITLE") {
@@ -388,9 +333,7 @@ const styleElement = (
   return text;
 };
 
-const content = (
-  element: any
-): string | undefined => {
+const content = (element: any): string | undefined => {
   const textRun = element?.textRun;
   const text = textRun?.content;
   if (textRun?.textStyle?.link?.url)
---
+++ util.ts
@@ -0,0 +1,196 @@
+import ClientOAuth2 from "client-oauth2";
+import { nodes, state, root } from "membrane";
+import fetch from "node-fetch";
+
+type Method = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+
+export async function api(
+  method: Method,
+  domain: string,
+  path: string,
+  query?: any,
+  body?: string
+) {
+  if (query) {
+    Object.keys(query).forEach((key) =>
+      query[key] === undefined ? delete query[key] : {}
+    );
+  }
+  const querystr =
+    query && Object.keys(query).length ? `?${new URLSearchParams(query)}` : "";
+
+  // The HTTP node to use. Either Membrane's authenticated HTTP node or the regular one we have an access token.
+  let req = { method, url: `https://${domain}/${path}${querystr}`, body };
+  if (state.accessToken) {
+    if (state.accessToken.expired()) {
+      console.log("Refreshing access token...");
+      state.accessToken = await state.accessToken.refresh();
+    }
+
+    req = state.accessToken.sign({ req });
+  }
+  return await fetch(req.url, { ...req, http: httpNode() });
+}
+
+export function usingUserApiKey() {
+  return state.clientId && state.clientSecret;
+}
+
+export async function authStatus() {
+  if (usingUserApiKey()) {
+    if (state.accessToken) {
+      return `Ready`;
+    } else {
+      return `Client ID/Secret configured. [Sign In](${await endpointUrl()}).`;
+    }
+  } else {
+    if (await authenticatedNode().hasAuthenticated.$get()) {
+      return `Ready`;
+    } else {
+      return `[Sign In](${await endpointUrl()})`;
+    }
+  }
+}
+
+// The HTTP node to use, depends on whether we have a user-provided API key or not
+function httpNode(): NodeGref<http.Root | http.Authenticated> {
+  if (usingUserApiKey()) {
+    return nodes.http;
+  } else {
+    return authenticatedNode();
+  }
+}
+
+function authenticatedNode(): NodeGref<http.Authenticated> {
+  return nodes.http.authenticated({
+    api: "google-docs",
+    authId: root.authId,
+  });
+}
+
+export async function endpoint({ args: { path, query, headers, body } }) {
+  const link = await nodes.http
+    .authenticated({ api: "google-docs", authId: root.authId })
+    .createLink.$invoke();
+  switch (path) {
+    case "/": {
+      return html(indexHtml(link, "auth"));
+    }
+    case "/auth":
+    case "/auth/": {
+      if (!state.clientId || !state.clientSecret) {
+        return JSON.stringify({ status: 303, headers: { location: "/" } });
+      }
+      await createAuthClient();
+      const url = state.auth.code.getUri({
+        query: { access_type: "offline", prompt: "consent" }, // Request refresh token
+        // TODO: Use OAuth state
+      });
+      return JSON.stringify({ status: 303, headers: { location: url } });
+    }
+    case "/auth/callback": {
+      state.accessToken = await state.auth.code.getToken(`${path}?${query}`);
+      if (state.accessToken?.accessToken) {
+        return html(`Driver configured!`);
+      }
+      return html(
+        `There was an issue acquiring the access token. Check the logs.`
+      );
+    }
+    default:
+      return JSON.stringify({ status: 404, body: "Not found" });
+  }
+}
+
+// Helper function to produce nicer HTML
+function html(body: string) {
+  // <link rel="stylesheet" href="https://unpkg.com/bootstrap@4.1.0/dist/css/bootstrap-reboot.css">
+  return `
+  <!DOCTYPE html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Google Calendar Driver for Membrane</title>
+    <link rel="stylesheet" href="https://www.membrane.io/light.css"></script>
+  </head>
+  <body>
+    <div style="position: absolute; inset: 0px; display: flex; flex-direction: row; justify-content: center; align-items: center;">
+      <div style="display: flex; flex-direction: column; align-items: center; max-width: 800px;">
+        ${body}
+      </div>
+    </div>
+  </body>
+  `;
+}
+
+// Offer the option to use the user's own API key/secret or use Membrane's authentication
+function indexHtml(membraneAuthUrl: string, customAuthUrl: string) {
+  return html(`
+  <div style="display: flex; flex-direction: row; justify-content: space-around; align-items: center; margin-bottom: 18px;">
+    <img style="width: 40px; height: 40px; margin-right: 10px;" src="https://ssl.gstatic.com/calendar/images/dynamiclogo_2020q4/calendar_12_2x.png"/>
+    <h1>Google Calendar Driver for Membrane</h1>
+  </div>
+  <div style="display: flex; flex-direction: row; justify-content: space-around; align-items: center;">
+    <section style="flex: 1; justify-content: space-between; align-items: center;">
+      <h2>Membrane authentication</h2>
+      <p>Use Membrane's Google Docs integration to sign-in. This is the recommended method.</p>
+      <a class="button" href="${membraneAuthUrl}">Sign in with Google</a>
+      <p> </p>
+    </section>
+    Or
+    <section style="flex: 1; justify-content: center; align-items: center;">
+      <h2>Bring your own Key</h2>
+      <p>Alternatively, use your own Google Client ID and Secret. This is more complicated but useful if you already have a Google Cloud account or want to use this API beyond Membrane's limits.</p>
+      ${
+        state.clientId && state.clientSecret
+          ? "<p>✅ Client ID and Secret configured</p>" +
+            (state.accessToken
+              ? "<p>✅ User signed-in</p>"
+              : "<p>❌ User <b>not signed-in</b></p>") +
+            `<p><a class="button" href=${customAuthUrl}/>Sign in with Google</a></p>`
+          : "<p>Client ID and Secret <b>not configured</b>.</p> <p>Invoke <code>:configure</code> with your Google Calendar API Key and Secret to authenticate</p>"
+      }
+    </section>
+  </div>
+  `);
+}
+
+export async function createAuthClient() {
+  const { clientId, clientSecret } = state;
+  if (clientId && clientSecret) {
+    state.auth = new ClientOAuth2(
+      {
+        clientId,
+        clientSecret,
+        accessTokenUri: "https://oauth2.googleapis.com/token",
+        authorizationUri: "https://accounts.google.com/o/oauth2/v2/auth",
+        redirectUri: `${await endpointUrl()}/auth/callback`,
+        // The 'drive.readonly' scope is required to list Google Docs files
+        scopes: [
+          "https://www.googleapis.com/auth/documents",
+          "https://www.googleapis.com/auth/drive.readonly",
+        ],
+      },
+      oauthRequest
+    );
+  }
+}
+
+// Cached endpoint URL since it's immutable
+export async function endpointUrl() {
+  if (!state.endpointUrl) {
+    state.endpointUrl = await nodes.process.endpointUrl.$get();
+  }
+  return state.endpointUrl;
+}
+
+async function oauthRequest(
+  method: string,
+  url: string,
+  reqBody: string,
+  headers: any
+) {
+  const res = await fetch(url, { body: reqBody.toString(), headers, method });
+  const status = res.status;
+  const body = await res.text();
+  return { status, body };
+}
